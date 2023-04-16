733fb4d67d91ce122041d40598b9e5abd43e5c07
Jonatas <djonatas31@gmail.com>
2023-04-16 16:53:36 -0300

    update

---
+++ main.py
@@ -0,0 +1,41 @@
+import random
+import os
+
+from palavrasEDicas import *
+
+numeroSecreto = random.randint(0, 9)
+PalavraSecreta = palavras[numeroSecreto]
+DicaSecreta = dicas[numeroSecreto]
+
+letrasDigitadas = []
+
+def clear():
+    os.system("cls")
+
+while True:
+    print("=-" * 30)
+    print(f"Dica: {DicaSecreta}")
+    letraDigitada = input("Digite uma letra: ")
+    if len(letraDigitada) != 1 or letraDigitada.isdigit():
+    
+        print("Digite apenas uma LETRA!")
+        continue
+
+    if letraDigitada not in letrasDigitadas:
+        letrasDigitadas.append(letraDigitada)
+
+    palavraFormada = ""
+    for letra in PalavraSecreta:
+        if letra in letrasDigitadas:
+            palavraFormada += letra
+        else:
+            palavraFormada += "*"
+    
+    clear()
+    if palavraFormada == PalavraSecreta:
+        
+        print("🎉Parabéns! Você ganhou!🎉")
+        break
+    else:
+        print(palavraFormada)
+    
---
+++ palavrasEDicas.py
@@ -0,0 +1,15 @@
+palavras = [
+    'bicicleta', 'computador', 'oculos', 'televisao', 'piano', 'sorriso', 'viagem', 'amizade', 'livro', 'futebol'
+]
+dicas = [
+    'Um meio de transporte que utiliza pedais para se locomover.',
+    'Uma máquina eletrônica capaz de processar dados e executar programas.',
+    'Um acessório utilizado para melhorar a visão ou para proteger os olhos.',
+    'Um aparelho eletrônico utilizado para assistir a programas de TV.',
+    'Um instrumento musical de cordas percutidas.',
+    'Expressão facial que mostra felicidade ou alegria.',
+    'Deslocamento de um lugar para outro.',
+    'Relação afetiva entre duas pessoas.',
+    'Uma obra literária ou científica com um conjunto de páginas encadernadas.',
+    'Um esporte de equipe jogado com uma bola em um campo retangular.'
+]
